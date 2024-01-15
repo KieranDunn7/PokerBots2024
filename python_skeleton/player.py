3251665c956ca7d4facd8c55912cb536ac11e3a5
peytonjw52 <151202707+peytonjw52@users.noreply.github.com>
2024-01-14 21:47:54 -0500

    Update player.py

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -171,38 +171,36 @@ class Player(Bot):
         continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
         my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
         opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
-        if RaiseAction in legal_actions:
-            min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
-            min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
-            max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
-            
-        if street == 0:
-            pfp = self.pfp
-            if not self.big_blind:
-                if pfp < 0.52:
-                    return FoldAction()
-                return CallAction()
-            if self.big_blind:
-                if opp_pip > 8:
-                    alpha = 0.8
-                    beta = 0.72
-                alpha = 0.72
-                beta = 0.6
-                if pfp < beta:
-                    return FoldAction()
-                if pfp < alpha:
-                    return CallAction()
-                if RaiseAction in legal_actions:
-                    return RaiseAction(random.randint(min_raise, 2*min_raise))
-                return CallAction()
-                
-                    
-                
         if BidAction in legal_actions:
             pass
-        if CheckAction in legal_actions:
-            return CheckAction()
-        return FoldAction()
+        else:
+            if RaiseAction in legal_actions:
+                min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
+                min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
+                max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
+                
+            if street == 0:
+                pfp = self.pfp
+                if not self.big_blind:
+                    if pfp < 0.52:
+                        return FoldAction()
+                    return CallAction()
+                if self.big_blind:
+                    if opp_pip > 8:
+                        alpha = 0.8
+                        beta = 0.72
+                    alpha = 0.72
+                    beta = 0.6
+                    if pfp < beta:
+                        return FoldAction()
+                    if pfp < alpha:
+                        return CallAction()
+                    if RaiseAction in legal_actions:
+                        return RaiseAction(random.randint(min_raise, 2*min_raise))
+                    return CallAction()
+            if CheckAction in legal_actions:
+                return CheckAction()
+            return FoldAction()
 
 
 if __name__ == '__main__':
