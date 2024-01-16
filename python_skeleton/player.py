e5ec6b25c0ac621619758949b8408b1895fc71a2
peytonjw52 <151202707+peytonjw52@users.noreply.github.com>
2024-01-15 22:16:22 -0500

    Update player.py

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -374,9 +374,9 @@ class Player(Bot):
                 self.folded = True
                 return FoldAction()
             elif self.prob_win > random.uniform(prob2, prob3):
-                return RaiseAction(min(max_raise, int(random.uniform(min_raise, min(raise1*min_raise, max_raise))))
+                return RaiseAction(min(max_raise, int(random.uniform(min_raise, min(raise1*min_raise, max_raise)))))
             elif self.prob_win > random.uniform(prob4, prob5) and my_pip == 0:
-                return RaiseAction(min(max_raise, int(random.uniform(min_raise, min(raise2*min_raise, max_raise))))
+                return RaiseAction(min(max_raise, int(random.uniform(min_raise, min(raise2*min_raise, max_raise)))))
             if CheckAction in legal_actions:
                 return CheckAction()
             return CallAction()
