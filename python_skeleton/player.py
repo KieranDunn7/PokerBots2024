5cd933827068f8636618acd356b62473d860d30d
peytonjw52 <151202707+peytonjw52@users.noreply.github.com>
2024-01-16 14:44:06 -0500

    Update player.py

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -195,6 +195,7 @@ class Player(Bot):
         self.suit1, self.suit2 = suit1, suit2
         if not self.forfeit and not self.opp_forfeit:
             print(f"Round #{round_num}")
+            print()
         if round_num == NUM_ROUNDS:
             print("opp_bids =", self.opp_bids)
             print("bid_pot_sizes =", self.bid_pot_sizes)
