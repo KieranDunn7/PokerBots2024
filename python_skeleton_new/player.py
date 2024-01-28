56ac9092c9e33c2d56a4f1b20f22cef1eddfb044
Peyton Worthington <peytonworthington@dhcp-10-31-183-164.dyn.MIT.EDU>
2024-01-27 19:22:24 -0500

    mm

--- python_skeleton_new/player.py
+++ python_skeleton_new/player.py
@@ -495,7 +495,7 @@ class Player(Bot):
                 opp_bid_avg = self.opp_bid_avg
                 opp_bid_stdv = self.opp_bid_var**(1/2)
             bid = int(opp_bid_avg + opp_bid_stdv * 1.96 * (diff-0.3) * 10) - 1
-            return BidAction(min(my_stack, max(bid, 25)))
+            return BidAction(min(150, my_stack, max(bid, 25)))
         
         if self.all_in:
             return CheckAction()
